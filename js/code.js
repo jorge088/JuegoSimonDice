fb54115456c300902e9939d846ab913d70fdc5c7
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-26 00:48:05 -0300

    Agrego funcionalidad al juego

--- js/code.js
+++ js/code.js
@@ -1,21 +1,93 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    //código a ejecutar cuando el DOM está listo para recibir acciones
-    var app = new Aplicacion();
+    //código a ejecutar cuando el DOM está listo para recibir acciones    
+    juego =new Juego();
+    var app = new Aplicacion(juego);
     document.getElementById('btn-jugar').disabled=true;
 
     document.getElementById('btn-cargarDatos').addEventListener('click',function(){
         app.evtoCargarDatos();
     });
     document.getElementById('btn-jugar').addEventListener('click',function(){
-
+        app.iniciar();
     });
 });
 
 class Aplicacion {
-    constructor(){
-        //this.juego=juego;
+    
+    constructor(juego){
+        this.juego=juego;
         document.getElementById("lblResultado").textContent="";
         document.getElementById("lblNivel").textContent="";
+    }
+
+    iniciar(){
+        this.evtoGenerarCuadroAleatorio();
+        document.getElementById('lblNivel').textContent=this.juego.nivel;
+        this.evtoElegirCuadro();
+    }
+
+    evtoGenerarCuadroAleatorio(){
+        this.juego.secuencia = [];
+        this.juego.secuenciaJugador = [];
+        this.juego.generarCuadroAleatorio;
+        Aplicacion.mostrarSecuencia();
+    }
+
+    static mostrarSecuencia(){
+        let indice = 0;
+        let resaltado = false;
+        var proceso = setInterval(function(){
+            if (indice < juego.secuencia.length){
+                document.getElementById('lblResultado').textContent='¡JUEGO EN EJECUCIÓN!';
+                if(resaltado==false){
+                    resaltado = true;
+                    document.getElementById(`cuadro${juego.secuencia[indice]}`).classList.add("resaltado");    
+                }else{
+                    resaltado =false
+                    document.getElementById(`cuadro${juego.secuencia[indice]}`).classList.remove("resaltado");
+                    indice++;
+                }
+            }else{
+                clearInterval(proceso);
+                document.getElementById('lblResultado').textContent='Su turno';
+            }
+        },700);
+    }
+
+    evtoElegirCuadro(){
+        let cuadroVioleta = document.getElementById('cuadro0');
+        let cuadroRojo = document.getElementById('cuadro1');
+        let cuadroVerde = document.getElementById('cuadro2');
+        let cuadroAzul = document.getElementById('cuadro3');
+        //Animacion al clickear caja
+        cuadroVioleta.addEventListener('click',function(){
+            juego.cuadroElegido = 0; //Envio caja seleccionada a clase Juego
+            cuadroVioleta.classList.add('resaltado');
+            setTimeout(function(){
+                cuadroVioleta.classList.remove('resaltado');
+            },500); 
+        });
+        cuadroRojo.addEventListener('click',function(){
+            juego.cuadroElegido = 1; //Envio caja seleccionada a clase Juego
+            cuadroRojo.classList.add('resaltado');
+            setTimeout(function(){
+                cuadroRojo.classList.remove('resaltado');
+            },500);
+        });
+        cuadroVerde.addEventListener('click',function(){
+            juego.cuadroElegido = 2; //Envio caja seleccionada a clase Juego
+            cuadroVerde.classList.add('resaltado');
+            setTimeout(function(){
+                cuadroVerde.classList.remove('resaltado');
+            },500);
+        });
+        cuadroAzul.addEventListener('click',function(){
+            juego.cuadroElegido = 3; //Envio caja seleccionada a clase Juego
+            cuadroAzul.classList.add('resaltado');
+            setTimeout(function(){
+                cuadroAzul.classList.remove('resaltado');
+            },500);
+        });
     }
 
     evtoCargarDatos() {
@@ -24,38 +96,34 @@ class Aplicacion {
         myModal.show();
         document.getElementById('inputUsuario-validation').style.opacity=0;
         document.getElementById('inputCorreo-validation').style.opacity=0;
-        let close=true;
-        //Validación de datos
 
+        //Validación de datos
         document.getElementById('btn-login').addEventListener( 'click' , function(){
-            let usuarioVal =false
-            let correoVal=false;
+            let usuarioVal = false
+            let correoVal = false;
             if(document.getElementById('inputUsuario').value != ""){
-                usuarioVal=true;
-                document.getElementById('inputUsuario-validation').style.opacity=0;
+                usuarioVal = true;
+                document.getElementById('inputUsuario-validation').style.opacity = 0;
             }else{
-                document.getElementById('inputUsuario-validation').style.opacity=1;
+                document.getElementById('inputUsuario-validation').style.opacity = 1;
             }
             let correo = document.getElementById('inputCorreo').value;
-            if(correo.indexOf("@")!=(-1)&&correo.indexOf(".com")!=(-1)){
-                correoVal=true;
-                document.getElementById('inputCorreo-validation').style.opacity=0;
+            if(correo.indexOf("@") != (-1)&&correo.indexOf(".com") != (-1)){
+                correoVal = true;
+                document.getElementById('inputCorreo-validation').style.opacity = 0;
             }else{
-                document.getElementById('inputCorreo-validation').style.opacity=1;
+                document.getElementById('inputCorreo-validation').style.opacity = 1;
             }
             if(correoVal && usuarioVal){
                 myModal.hide();
                 //Habilita el boton para jugar :v
-                document.getElementById('btn-jugar').disabled=false;
+                document.getElementById('btn-jugar').disabled = false;
                 //Muestra los datos ingresados//
                 document.getElementById('lblJugador').textContent = document.getElementById('inputUsuario').value;
                 document.getElementById('lblCorreo').textContent = document.getElementById('inputCorreo').value;
                 let fechahoy= new Date();
-                let fechahoyanio = fechahoy.getFullYear();
-                let fechahoymes = fechahoy.getMonth()+1;
-                let fechahoydia = fechahoy.getDate();
-                let fechafinal = String(fechahoydia + "/" + fechahoymes + "/" + fechahoyanio);
-                document.getElementById('lblFecha').textContent=fechafinal;
+                let fechafinal = String(fechahoy.getDate() + "/" + fechahoy.getMonth()+1 + "/" + fechahoy.getFullYear());
+                document.getElementById('lblFecha').textContent = fechafinal;
             }
         });
     }
--- js/modelo.js
+++ js/modelo.js
@@ -1,5 +1,58 @@
 class Juego{
      constructor(){
-         
+          var jugador = new Jugador();
+          var jugadores =[];
+          this.nivel=1;
+          this.jugadores=jugadores;
+          this.jugador= jugador;
+          this.secuencia = [];
+          this.secuenciaJugador = [];
+     }
+     get generarCuadroAleatorio(){
+          //Genero el cuadro aleatorio.
+          let cuadroAleat = Math.trunc(Math.random()*4);
+          //guardo en array de secuencia
+          this.secuencia.push(cuadroAleat);
+          return cuadroAleat;
+     }
+     set cuadroElegido(indice){
+          this.secuenciaJugador.push(indice);
+          var sigueJugando = false;
+          
+          for(let i =0 ;i < this.secuenciaJugador.length;i++){
+               console.log(i);
+               if(this.secuencia[i] != this.secuenciaJugador[i]){
+                    document.getElementById('lblResultado').textContent = "FIN DEL JUEGO";
+                    break;
+               }else{
+                    if(i==this.secuenciaJugador.length-1 ){
+                         console.log("Cambia condicion sigue jugando!");
+                         console.log(this.secuencia);
+                         console.log(this.secuenciaJugador);
+                         sigueJugando = true;
+                    }
+               }
+          }
+          if(sigueJugando){
+               if(this.secuenciaJugador.length == this.secuencia.length){
+                    this.nivel++;
+                    this.secuenciaJugador = [];
+                    this.generarCuadroAleatorio;
+                    document.getElementById("lblResultado").textContent= "¡BIEN HECHO!";
+                    setTimeout(function(){
+                         document.getElementById('lblNivel').textContent=this.juego.nivel;
+                         Aplicacion.mostrarSecuencia();
+                    },2000);
+               }
+          }
      }
 }
+class Jugador{
+     constructor(ranking,nombre,pais,puntaje,fecha,correo){
+         this.ranking = ranking;
+         this.nombre = nombre;
+         this.puntaje = puntaje;
+         this.fecha = fecha;
+         this.correo = correo;
+     }
+ }
