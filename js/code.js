4141d2e3410b5746cb7b88ea4a934947a072ebfe
Jorge Mendez <mendezjorge98@gmail.com>
2022-01-26 23:05:34 -0300

    agrego comentarios a codigo js

--- js/code.js
+++ js/code.js
@@ -1,19 +1,18 @@
 class Aplicacion {
     constructor(juego){
         this.juego=juego;
-        document.getElementById("lblResultado").textContent="";
-        document.getElementById("lblNivel").textContent="";
+        document.getElementById("lblResultado").textContent="---";
+        document.getElementById("lblNivel").textContent="---";
     }
 
     iniciar(){
         this.juego.secuencia = [];
         this.juego.secuenciaJugador = [];
         this.juego.nivel=1;
+        document.getElementById('lblNivel').textContent=this.juego.nivel;
         document.getElementById('btn-jugar').disabled=true;
         this.evtoGenerarCuadroAleatorio();
-        document.getElementById('lblNivel').textContent=this.juego.nivel;
         //this.agregarEvtosClick();
-
     }
 
     evtoGenerarCuadroAleatorio(){
@@ -24,8 +23,8 @@ class Aplicacion {
     static mostrarSecuencia(){
         let indice = 0;
         let resaltado = false;
-        app.eliminarEvtosClick();
-        var proceso = setInterval(function(){
+        app.eliminarEvtosClick(); //quito eventos de click, de niveles anteriores
+        var proceso = setInterval(function(){ //muestra la secuencia entera, cada 600ms
             if (indice < juego.secuencia.length){
                 document.getElementById('lblResultado').textContent='¡JUEGO EN EJECUCIÓN!';
                 if(resaltado==false){
@@ -34,12 +33,12 @@ class Aplicacion {
                 }else{
                     resaltado =false
                     document.getElementById(`cuadro${juego.secuencia[indice]}`).classList.remove("resaltado");
-                    indice++;
+                    indice++; //luego de hacer iluminar el cuadro aumenta de posicion en el array de secuencia.
                 }
             }else{
                 clearInterval(proceso);
                 document.getElementById('lblResultado').textContent='Su turno';
-                app.agregarEvtosClick();
+                app.agregarEvtosClick(); //terminada la secuencia, agrego eventos click
             }
         },600);
     }
@@ -50,40 +49,46 @@ class Aplicacion {
         document.getElementById('cuadro2').addEventListener('click',this.elegirCuadro);
         document.getElementById('cuadro3').addEventListener('click',this.elegirCuadro);
     }
+
     eliminarEvtosClick(){
         document.getElementById('cuadro0').removeEventListener('click',this.elegirCuadro);
         document.getElementById('cuadro1').removeEventListener('click',this.elegirCuadro);
         document.getElementById('cuadro2').removeEventListener('click',this.elegirCuadro);
         document.getElementById('cuadro3').removeEventListener('click',this.elegirCuadro);
     }
+
     elegirCuadro(event){
-        //capturo desde el evento el id del cuadro clickeado
+        //capturo desde el evento, el id del cuadro clickeado
         let cuadroElegido = Number(event.target.id[event.target.id.length-1]);
         //Resalto el cuadro seleccionado
         document.getElementById(`cuadro${cuadroElegido}`).classList.add('resaltado');
-            setTimeout(function(){
-                document.getElementById(`cuadro${cuadroElegido}`).classList.remove('resaltado');
-            },400);
-        juego.cuadroElegido = cuadroElegido; //Envio caja seleccionada a clase Juego
+        setTimeout(function(){
+            document.getElementById(`cuadro${cuadroElegido}`).classList.remove('resaltado');
+        },400);
+
+        juego.cuadroElegido = cuadroElegido; //Envio numero del cuadro clickeado para validar.
     }
 
     evtoCargarDatos() {
         //Muestro el modal de ingreso
         var myModal = new bootstrap.Modal(document.getElementById("modalIngreso"), {});
         myModal.show();
+        //oculto mensajes de error en validación
         document.getElementById('inputUsuario-validation').style.opacity=0;
         document.getElementById('inputCorreo-validation').style.opacity=0;
 
-        //Validación de datos
+        //Validación de datos al hacer click en btn login
         document.getElementById('btn-login').addEventListener( 'click' , function(){
             let usuarioVal = false
             let correoVal = false;
+            //validacion de usuario
             if(document.getElementById('inputUsuario').value != ""){
                 usuarioVal = true;
                 document.getElementById('inputUsuario-validation').style.opacity = 0;
             }else{
                 document.getElementById('inputUsuario-validation').style.opacity = 1;
             }
+            //validacion de correo
             let correo = document.getElementById('inputCorreo').value;
             if(correo.indexOf("@") != (-1)&&correo.indexOf(".com") != (-1)){
                 correoVal = true;
@@ -91,13 +96,15 @@ class Aplicacion {
             }else{
                 document.getElementById('inputCorreo-validation').style.opacity = 1;
             }
+            //usuario y correo son validos:
             if(correoVal && usuarioVal){
                 myModal.hide();
-                //Habilita el boton para jugar :v
+                //Habilita el boton para jugar 
                 document.getElementById('btn-jugar').disabled = false;
-                //Muestra los datos ingresados//
+                //Muestra los datos ingresados en caja de informacion//
                 document.getElementById('lblJugador').textContent = document.getElementById('inputUsuario').value;
                 document.getElementById('lblCorreo').textContent = document.getElementById('inputCorreo').value;
+                //cargo fecha actual
                 let fechahoy= new Date();
                 let fechafinal = String(fechahoy.getDate() + "/" + fechahoy.getMonth()+1 + "/" + fechahoy.getFullYear());
                 document.getElementById('lblFecha').textContent = fechafinal;
@@ -105,12 +112,15 @@ class Aplicacion {
         });
     }
 }
+
 var juego =new Juego();
 var app = new Aplicacion(juego);
 document.getElementById('btn-jugar').disabled=true;
+
 document.getElementById('btn-cargarDatos').addEventListener('click',function(){
     app.evtoCargarDatos();
 });
+
 document.getElementById('btn-jugar').addEventListener('click',function(){
     app.iniciar();
 });
--- js/modelo.js
+++ js/modelo.js
@@ -1,25 +1,30 @@
 class Juego{
+
      constructor(){
           this.nivel=1;
           this.secuencia = [];
           this.secuenciaJugador = [];
      }
+     
      get generarCuadroAleatorio(){
-          //Genero el cuadro aleatorio.
+          //Genero un num aleatorio entre 0 y 3.
           let cuadroAleat = Math.trunc(Math.random()*4);
           //guardo en array de secuencia
           this.secuencia.push(cuadroAleat);
           return cuadroAleat;
      }
+
      set cuadroElegido(indice){
           this.secuenciaJugador.push(indice);
           var sigueJugando = false;
-          
+          //compara secuencia y secuencia del jugador
           for(let i =0 ;i < this.secuenciaJugador.length;i++){
                if(this.secuencia[i] != this.secuenciaJugador[i]){
+                    //son distintos en alguna posicion, fin del juego
                     document.getElementById('lblResultado').textContent = "FIN DEL JUEGO";
                     app.eliminarEvtosClick();
                     document.getElementById('btn-jugar').disabled=false;
+                    //muestro modal de GameOver
                     let myModal = new bootstrap.Modal(document.getElementById("modalGameOver"), {});
                     myModal.show();
                     document.getElementById("btn-continuar").addEventListener('click',function(){
@@ -28,19 +33,20 @@ class Juego{
                     break;
                }else{
                     if(i==this.secuenciaJugador.length-1 ){
+                         //se recorre toda la secuencia de jugador
                          sigueJugando = true;
                     }
                }
           }
           if(sigueJugando){
-               if(this.secuenciaJugador.length == this.secuencia.length){
+               if(this.secuenciaJugador.length == this.secuencia.length){//si no son iguales, es porque falta clickear cuadros
+                    //Paso de nivel, quito eventos click
                     app.eliminarEvtosClick();
                     this.nivel++;
                     this.secuenciaJugador = [];
                     this.generarCuadroAleatorio;
                     document.getElementById("lblResultado").textContent= "¡BIEN HECHO!";
-                    setTimeout(function(){
-                         
+                    setTimeout(function(){ //espera 1300ms para iniciar sig nivel
                          document.getElementById('lblNivel').textContent=this.juego.nivel;
                          Aplicacion.mostrarSecuencia();
                          
@@ -48,5 +54,4 @@ class Juego{
                }
           }
      }
-     
 }
